3413323e868a47d01000c09e524f7e26a6e33dc8
Sonali Bhadra <sos.glu16@outlook.com>
2025-04-05 22:36:52 +0530

    upd

---
+++ 2215. Find the Difference of Two Arrays.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        Set<Integer> set1=new HashSet<>();
+        Set<Integer> set2=new HashSet<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        ans.add(new ArrayList<>());
+        ans.add(new ArrayList<>());
+        for (int i=0; i<nums1.length;i++){
+            set1.add(nums1[i]);
+        }
+        for (int i=0;i<nums2.length;i++){
+            set2.add(nums2[i]);
+        }
+        for(int i : set1){
+            if(!set2.contains(i)){
+                ans.get(0).add(i);
+            }
+        }
+        for (int i : set2){
+            if(!set1.contains(i)){
+                ans.get(1).add(i);
+            }
+        }
+        return ans;
+    }
+}
